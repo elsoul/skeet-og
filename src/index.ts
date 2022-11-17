169d3bf6bfa66eb58cdae1d09544eb43ca484249
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2022-11-17 22:16:04 +0100

    add commander

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import Dotenv from "dotenv";
+import { Command } from "commander";
+const program = new Command();
+
 Dotenv.config();
-console.log("running skeet!")
+
+async function run() {
+	console.log("running skeet!");
+}
+
+async function create() {
+	console.log("creating skeet!");
+}
+
+async function main() {
+	program.command("run").action(run);
+	program.command("create").action(create);
+	await program.parseAsync(process.argv);
+}
+
+main();
