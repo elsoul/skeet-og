4fdc5ab23b87ef95447bd87f5b5c5ae5a1a4ce5b
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-01-29 08:45:14 +0100

    add dockerLogin

--- src/cli/git/addJsonEnv.ts
+++ src/cli/git/addJsonEnv.ts
@@ -1,7 +1,7 @@
 import { exec } from 'node:child_process'
+import { KEYFILE_PATH } from '@/lib/getNetworkConfig'
 
 export const addJsonEnv = async () => {
-  const filePath = KEYFILE_PATH
-  const cmdLine = `gh secret set SKEET_GCP_SA_KEY < ${filePath}`
+  const cmdLine = `gh secret set SKEET_GCP_SA_KEY < ${KEYFILE_PATH}`
   exec(cmdLine)
 }
