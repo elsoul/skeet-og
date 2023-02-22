43762d6873dd877e534c37e3786ce4409688313e
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-02-22 11:01:31 +0100

    add docker build platform

--- src/cli/deploy/deploy.ts
+++ src/cli/deploy/deploy.ts
@@ -154,6 +154,8 @@ export const cloudRunBuild = async (
   const shCmd = [
     'docker',
     'build',
+    '--platform',
+    'linux/amd64',
     '-f',
     `${buildPath}/Dockerfile`,
     buildPath,
