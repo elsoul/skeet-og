db969781f2464cbf0f0a6a1293f567a21da2d765
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-02-24 19:27:39 +0100

    add actions test

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.ts', '!**/node_modules/**'],
+  coverageDirectory: 'coverage_dir',
+  coverageReporters: ['html'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['./tests/jest.setup.ts'],
+}
---
+++ tests/check.test.ts
@@ -0,0 +1,5 @@
+import { skeetEnv } from './jest.setup'
+test('check', () => {
+  console.log(skeetEnv)
+  console.log('OK')
+})
