9d8e0707e445b1bb97d197c1217aa1040ae0c374
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-02-27 11:48:56 +0100

    fix add ip

--- src/cli/sql/sqlIp.ts
+++ src/cli/sql/sqlIp.ts
@@ -1,13 +1,19 @@
 import { SkeetCloudConfig } from '@/types/skeetTypes'
 import { patchSQL } from '@/cli'
 import { importConfig } from '@/index'
+import { getNetworkConfig } from '@/lib/getNetworkConfig'
 
 export const sqlIp = async () => {
   const skeetCloudConfig: SkeetCloudConfig = await importConfig()
+  const { networkName } = await getNetworkConfig(
+    skeetCloudConfig.api.projectId,
+    skeetCloudConfig.api.appName
+  )
   await patchSQL(
     skeetCloudConfig.api.projectId,
     skeetCloudConfig.api.appName,
     '',
-    skeetCloudConfig.api.db.whiteList
+    skeetCloudConfig.api.db.whiteList,
+    networkName
   )
 }
--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,1 +1,1 @@
-export const VERSION = '0.30.4'
+export const VERSION = '0.30.5'
