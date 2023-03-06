405bd6222cd739e88fa501e8758a045e99c1c083
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-03-06 11:12:32 +0100

    update add worker plugin

--- src/cli/add/addWorkerPlugin.ts
+++ src/cli/add/addWorkerPlugin.ts
@@ -1,48 +1,33 @@
 import { Logger } from '@/lib/logger'
-import fs from 'fs'
-import { execSyncCmd } from '@/lib/execSyncCmd'
 import { importConfig } from '@/index'
 import {
-  WORKER_SOLANA_TRANSFER_REPO_URL,
-  WORKER_PATH,
-} from '@/lib/getNetworkConfig'
-import {
-  workerDeploy,
   setupActions,
   addWorkerEnv,
   updateSkeetCloudConfig,
   addWorkerToPackageJson,
+  getWorkerConfig,
+  cloudRunDeploy,
 } from '@/cli'
 
 export const addWorkerPlugin = async (pluginName: string) => {
   const skeetConfig = await importConfig()
   const workerName = pluginName
-  const workerDir = './apps/workers/' + workerName
-
-  if (fs.existsSync(workerDir)) {
-    await Logger.error(`Already exist workerName: ${workerName}!`)
-    return ''
-  } else {
-    fs.mkdir(workerDir, { recursive: true }, (err) => {
-      if (err) throw err
-    })
-  }
-  const gitCloneCmd = [
-    'git',
-    'clone',
-    WORKER_SOLANA_TRANSFER_REPO_URL,
-    workerDir,
-  ]
-  await execSyncCmd(gitCloneCmd)
-  const rmDefaultGit = ['rm', '-rf', '.git']
-  await execSyncCmd(rmDefaultGit, workerDir)
   await addWorkerEnv(workerName)
   await updateSkeetCloudConfig(workerName)
   await addWorkerToPackageJson(workerName)
   await setupActions()
-  const yarnCmd = ['yarn']
-  await execSyncCmd(yarnCmd, `${WORKER_PATH}/${workerName}`)
   const isWorkerPlugin = true
-  await workerDeploy(workerName, skeetConfig, isWorkerPlugin)
+  const workerConf = await getWorkerConfig(pluginName)
+  await cloudRunDeploy(
+    skeetConfig.api.projectId,
+    skeetConfig.api.appName,
+    skeetConfig.api.region,
+    workerConf.cloudRun.memory,
+    String(workerConf.cloudRun.cpu),
+    String(workerConf.cloudRun.maxInstances),
+    String(workerConf.cloudRun.minInstances),
+    pluginName,
+    isWorkerPlugin
+  )
   Logger.success(`Successfully created ${workerName}!`)
 }
--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,1 +1,1 @@
-export const VERSION = '0.34.2'
+export const VERSION = '0.34.3'
